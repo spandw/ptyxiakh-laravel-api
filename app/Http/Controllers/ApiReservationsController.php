881c2e740551f0ac6ca130f17c73545bcfadd247
spandw <spandonidisk@yahoo.gr>
2023-10-03 15:10:28 +0300

    made reservation function

--- app/Http/Controllers/ApiReservationsController.php
+++ app/Http/Controllers/ApiReservationsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\ParkingSpot;
 use App\Models\Reservation;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -53,15 +54,12 @@ class ApiReservationsController extends Controller
 
     public function checkAvailability(Request $request)
     {
-        // $date1 = Carbon::parse($check_start_date);
-        // $date2 = Carbon::parse($check_end_date);
-        // $daysDifference = $date2->diffInDays($date1);
-        // echo $daysDifference;
 
         $check_start_date = $request->start_date;
         $check_end_date = $request->end_date;
 
         $reservations = Reservation::checkDates($check_start_date, $check_end_date)->pluck('parking_spot_id');
+
 
         $available_spots = ParkingSpot::whereNotIn('id', $reservations)
             ->get();
@@ -70,6 +68,57 @@ class ApiReservationsController extends Controller
             'message' => "Reservations on the dates given.",
             'reservations' => $reservations,
             'available_spots' => $available_spots
+        ], 200);
+    }
+
+    public function createReservation(Request $request)
+    {
+        $check_start_date = $request->start_date;
+        $check_end_date = $request->end_date;
+
+        //dates se int
+        $int_start_date = Carbon::parse($check_start_date);
+        $int_end_date = Carbon::parse($check_end_date);
+
+        $daysDifference = $int_end_date->diffInDays($int_start_date);
+
+        $user_id = auth('sanctum')->user()->id;
+        $reservee = User::findOrFail($user_id);
+
+        $isNotAvailable = Reservation::checkDates($check_start_date, $check_end_date)->where('parking_spot_id', $request->id)->exists();
+        if ($isNotAvailable) {
+            return response()->json([
+                'message' => "There is already a reservation on those dates."
+            ], 422);
+        }
+
+        $reserveeCredits = $reservee->credits;
+        if ($reserveeCredits < $daysDifference) {
+            return response()->json([
+                'message' => "You have insufficient amount of credits",
+                'Your Credits' => $reserveeCredits
+            ], 422);
+        }
+        $parkingSpot = ParkingSpot::find($request->parking_spot_id);
+        $owner_id = $parkingSpot->user_id;
+
+        $owner = User::find($owner_id);
+        $ownerCredits = $owner->credits + $daysDifference;
+        $owner->update(['credits' => $ownerCredits]);
+
+        $reserveeCredits = $reserveeCredits - $daysDifference;
+        User::where('id', $user_id)->update(['credits' => $reserveeCredits]);
+
+        $reservation = Reservation::create([
+            'user_id' => $user_id,
+            'parking_spot_id' => $request->spotId,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+        ]);
+
+        return response()->json([
+            'message' => "Reservation created successfully",
+            'reservation' => $reservation,
         ], 200);
     }
 }
